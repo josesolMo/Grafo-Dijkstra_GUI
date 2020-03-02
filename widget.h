56b684d09b1095a4a9e6242ec9c86edcfefbc443
Jose Solano <joseandressolanomora@gmail.com>
2020-03-02 17:33:37 -0600

    Se documenta el código
    Se realiza la documentación de todo el código utilizando el formato de Doxygen

--- widget.h
+++ widget.h
@@ -1,25 +1,43 @@
+/**
+  * @file widget.h
+  * @title Widget
+  * @brief Extiende de QWidget, provee la interfaz gráfica que facilita el envio y recibo de información por parte del servidor
+  * @version 1.0
+  * @author JoseSol
+  * @date 02/03/2020
+  */
+
 #ifndef WIDGET_H
 #define WIDGET_H
 
 #include <QWidget>
+#include <QTcpSocket>
 
 namespace Ui {
 class Widget;
 }
-
-class QTcpSocket;
 
 class Widget : public QWidget
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor de Widget
+     * @param parent
+     */
     explicit Widget(QWidget *parent = nullptr);
     ~Widget();
 
 private slots:
+    /**
+     * @brief Función que se ejecuta cinado el botón "Enviar" se presiona
+     */
     void on_send_clicked();
 
+    /**
+     * @brief Función que se ejecuta cinado el botón "Conectar" se presiona
+     */
     void on_bind_clicked();
 
 private:
